c1dbe54195421cf6a4f6153ee6c6384cf12bc729
RobertMili <robertmilicevic3869@gmail.com>
2022-12-12 20:01:24 +0100

    Finish with day10

--- src/day10/Main.java
+++ src/day10/Main.java
@@ -9,10 +9,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        day10.readingFile.readingFIle((ArrayList<String>)input);
+        day10.readingFile.readingFIle((ArrayList<String>) input);
 
-        for (String s: input){
-            System.out.println(s);
+
+        int index = 0;
+        int delay = 0;
+        int register = 1;
+        int cycle = 0;
+        int part1 = 0;
+
+        while (index < input.size()){
+
+           String[] parts = input.get(index).split(" ");
+
+            switch (parts[0]){
+
+                case "noop" -> index++;
+
+                case "addx" ->
+                {
+                    if (delay == 0)
+                    {
+                        delay++;
+                    } else {
+                        register += Integer.parseInt(parts[1]);
+                        delay =0;
+                        index++;
+                    }
+                }
+            }
+            cycle++;
+
         }
     }
 }
+
